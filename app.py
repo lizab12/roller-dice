647b7d228f448da3b8745c7c3f7087a8afd99c45
liza6267 <lizaabhadkoliya36@gmail.com>
2025-10-17 00:07:52 -0500

    updated UI

--- app.py
+++ app.py
@@ -5,11 +5,11 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")  # make sure file name matches exactly
+    return render_template("index.html")
 
 @app.route('/roll')
 def roll_dice():
-    return jsonify({"result": random.randint(1,6)})
+    return jsonify({"result": random.randint(1, 6)})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=8080, debug=True)
